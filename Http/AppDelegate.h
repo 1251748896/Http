698a59737407d0913420ed10df55358fbeba5050
jiangbo <1251748896@qq.com>
2020-09-28 21:24:35 +0800

    Initial Commit

---
+++ Http/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Http
+//
+//  Created by HaoHuoBan on 2020/9/28.
+//  Copyright © 2020 HaoHuoBan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Http/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Http
+//
+//  Created by HaoHuoBan on 2020/9/28.
+//  Copyright © 2020 HaoHuoBan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
